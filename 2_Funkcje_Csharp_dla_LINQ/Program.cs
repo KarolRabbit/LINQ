477b1dfc39e455840a73da3071cc887bfb315823
Karol Króliczek <15zajc15@gmail.com>
2020-10-23 15:15:02 +0200

    Func i Action

--- 2_Funkcje_Csharp_dla_LINQ/Program.cs
+++ 2_Funkcje_Csharp_dla_LINQ/Program.cs
@@ -12,12 +12,29 @@ namespace _2_Funkcje_Csharp_dla_LINQ
     {
         static void Main(string[] args)
         {
+            //-------FUNC i ACTION-----------------------------------------------
+            double x = 5.5;
+            Func<double, double> square = (d => d * d);
+            Console.WriteLine($"Kwadrat z {x} jest równy {square(x)}");
+            double y = 25;
+            Func<double, double> squareRoot = (d => Math.Sqrt(d));
+            Console.WriteLine($"Pierwiastek kwadratowy z {y} jest równy {squareRoot(y)}");
+            string yourName = "Daniel";
+            Action<string> sayHello = (n => Console.WriteLine($"Osoba którą witamy to {n}"));
+            sayHello(yourName);
+            Console.WriteLine("-------------------------------------------------------------------");
+    //-------------------------------------------------------------------
+
             IEnumerable<Employee> programmers = new Employee[]
             {
                 new Employee{Id = 1, FirstName = "Karol", LastName = "Królik"},
                 new Employee{Id = 2, FirstName = "Michał", LastName = "Ciemniok"},
                 new Employee{Id = 3, FirstName = "Damian", LastName = "Bambaryła"},
                 new Employee{Id = 4, FirstName = "Anna", LastName = "Kolka"},
+                new Employee{Id = 1, FirstName = "Karol", LastName = "Gepheld"},
+                new Employee{Id = 2, FirstName = "Michał", LastName = "Dmowski"},
+                new Employee{Id = 3, FirstName = "Darek", LastName = "Botek"},
+                new Employee{Id = 4, FirstName = "Katarzyna", LastName = "Majewska"}
             };
 
             IEnumerable<Employee> drivers = new List<Employee>()
@@ -31,8 +48,9 @@ namespace _2_Funkcje_Csharp_dla_LINQ
             {
                 Console.WriteLine(person.FirstName);
             }
+            Console.WriteLine("-------------------------------------------------------------------");
             //---------------------Dodatkowe---------------------------------------------------
-            foreach (var person in programmers.Where(e=>e.LastName.StartsWith("K")))
+            foreach (var person in programmers.Where(p => p.FirstName.Length == 6).OrderBy(p => p.LastName))
             {
                 Console.WriteLine($"{person.LastName} {person.FirstName} nr ID: {person.Id}.");
             }
